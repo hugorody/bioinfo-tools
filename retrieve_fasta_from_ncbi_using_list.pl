e1da602094508ac5a7c685ac95584b10a7682d50
Hugo Rody <hugo.rody@unifesp.br>
2017-03-06 14:26:51 -0300

    Create retrieve_fasta_from_ncbi_using_list.pl

---
+++ retrieve_fasta_from_ncbi_using_list.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+use LWP::Simple;
+
+my $fasta = "";
+my $query = "";
+
+my $utils = 'http://www.ncbi.nlm.nih.gov/entrez/eutils/';
+my $dbase = 'nucleotide';
+my $reply = 'fasta';
+
+system ( "clear" );
+
+scalar ( @ARGV ) == 2 or die "\nNumber of parameters must be 2.\n\n\$ perl program_name input_file_name output_file_name\n\n";
+
+open F_INP, "<", $ARGV[0] or die "Can't open file '$ARGV[0]': $!";
+open F_OUT, ">", $ARGV[1] or die "Can't open file '$ARGV[1]': $!"; 
+
+while ( <F_INP> ) {
+        next if /^$/;    # Blank lines
+        next if /^\s+$/; # Lines with only space chars
+        next if /^\s*#/; # Just comments
+    
+        s/(.*)#.*/$1/;
+    
+        chomp; #exclui o controle de fim de linha
+        
+        $query = $utils . "efetch.fcgi?db=" . $dbase . "&id=" . $_ . "&rettype=" . $reply;
+        
+        $fasta = get ( $query );
+        
+        chomp ( $fasta );
+        
+        print F_OUT "$fasta\n";
+        
+        print "\n$query... done!\n";
+}
+
+close F_INP;
+close F_OUT;
+
+exit;
