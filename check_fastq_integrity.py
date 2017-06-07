9cc2f3ca15c5a975ee030001b271ad93230e6da2
Hugo Rody <hugo.rody@unifesp.br>
2017-06-07 17:22:05 -0300

    Update check_fastq_integrity.py

--- check_fastq_integrity.py
+++ check_fastq_integrity.py
@@ -19,7 +19,7 @@ for i in fgen:
     i = i.rstrip()
 
     if "@" in i[0]:
-        scafid = i.split()[0][1:]
+        scafid = i.split()[0][0:]
      
     if parsing > 0 and "@" not in i[0]:  #Second 2: if parsing is greater than 0, than this is the first line of fastq belonging to a read selected in the first step. When it finds a line with "@", then parsing will be set to 0 and loop starts again
         seqs[scafid] = seqs[scafid] + i
@@ -32,20 +32,36 @@ for i in fgen:
 
 #length seqs
 lenseqsdict = {} #dictionary containing read length as keys and the number of reads with that length as values
+lenquadict = {}
 for i in seqs.items():
     header= i[0]
-    info = i[1].split("+")
-    lenseq = len(info[0])
-    quality = info[1]
-    #print "@"+header+"\n"+seq.replace("+", "\n+\n")
+    if "+" in i[1]:
+        info = i[1].split("+")
+        lenseq = len(info[0])
+        lenqua = len(info[1])
+        #print "@"+header+"\n"+seq.replace("+", "\n+\n")
 
-    #feed length seqs dictionary
-    if lenseq not in lenseqsdict:
-        lenseqsdict[lenseq] = 1
+        #feed length seqs dictionary
+        if lenseq not in lenseqsdict:
+            lenseqsdict[lenseq] = 1
+        else:
+            newlen = int(lenseqsdict[lenseq]) + 1
+            lenseqsdict[lenseq] = newlen
+    
+        #feed length quality dictionary
+        if lenqua not in lenquadict:
+            lenquadict[lenqua] = 1
+        else:
+            newqua = int(lenquadict[lenqua]) + 1
+            lenquadict[lenqua] = newqua
+    
+        #check if nucleotides sequences have any character different from ATCG
+        nucleotides = ["A","T","C","G","N"]
+        checknucl = [x for x in list(info[0]) if x not in nucleotides]
+        if checknucl != []:
+            print "Header:",header,"Sequence:",info[0],"has no nucleotide character(s)."
     else:
-        newlen = int(lenseqsdict[lenseq]) + 1
-        lenseqsdict[lenseq] = newlen
-
+        print header,"has no valid sequence:"
 
 #PRINT RESULTS
 for i in lenseqsdict.items():
