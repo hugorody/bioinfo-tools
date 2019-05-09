f21798997055a03a018710d3d0e315a4dae3a77d
Hugo Rody <hugo.rody@unifesp.br>
2019-05-09 08:45:52 -0300

    Create run_TargetP.py

---
+++ run_TargetP.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+#Parse large FASTA files to TargetP software
+
+import sys
+import subprocess
+from Bio import SeqIO
+
+outputname = "file.targetP"
+
+targetPdic = {}
+seqs = []
+with open("file.fasta","r") as set1:
+#with open("test/one.fsa","r") as set1:
+    #for each sequence in fasta file
+    for j in SeqIO.parse(set1,"fasta"):
+        seqID = str(j.id)
+        seq = str(j.seq)
+        seqs.append(">"+seqID+"\n"+seq+"\n")
+
+x = 1
+iteration = 100
+count = 0
+for i in range(0, len(seqs), iteration):
+    outfasta = open("target_sequence.fas","w")
+    print ("Writing fasta")
+    if iteration*x < len(seqs):
+        maxval = iteration*x
+    else:
+        maxval = len(seqs)
+    for j in range(i, maxval):
+        outfasta.write(seqs[j])
+    x += 1
+    outfasta.close()
+    #run targetP
+    targetP = subprocess.Popen('./targetp -P target_sequence.fas > targetp_seq_result.tmp',shell = True)
+    targetP.wait()
+
+    #with result of targetP
+    with open("targetp_seq_result.tmp","r") as set_inside:
+        for i in set_inside:
+            i = i.rstrip().split()
+            if len(i) == 8 and i[0] != "Name":
+                targetPdic[count] = i
+                count += 1
+                print (" ".join(i))
+
+    #delete temporary targetp for sequence
+    targetP_tmp_del = subprocess.call('rm targetp_seq_result.tmp',shell = True)
+
+print ("Writing file targetP.result.txt")
+outtargetP = open(outputname,"w")
+outtargetP.write("Name                  Len     cTP    mTP     SP  other  Loc  RC\n"+
+                "----------------------------------------------------------------------\n")
+for i in targetPdic.items():
+    outtargetP.write("\t".join(i[1])+"\n")
+print ("Done!")
