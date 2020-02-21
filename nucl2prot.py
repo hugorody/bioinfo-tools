da30188159f3d7327ad83ec41a473a53493423a2
Hugo Rody <hugorody@usp.br>
2020-02-21 09:09:13 -0800

    Update nucl2prot.py

--- nucl2prot.py
+++ nucl2prot.py
@@ -6,7 +6,9 @@ from Bio import SeqIO
 
 fileFAS = sys.argv[1] #fasta file
 
+output = open(fileFAS+".nucl2prot.fas","w")
+
 with open(fileFAS,"r") as set2:
     for i in SeqIO.parse(set2, "fasta"):
         #print (i.format("fasta"))
-        print (">" + i.id + "\n" + i.seq.translate())
+        output.write (">" + str(i.id) + "\n" + str(i.seq.translate()).replace("\*",""))
