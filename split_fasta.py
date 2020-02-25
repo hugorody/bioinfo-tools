e8fd4b3d9a0394d701f65a149f7f3d1be52acf69
Hugo Rody <hugorody@usp.br>
2020-02-25 11:35:27 -0800

    Create split_fasta.py

---
+++ split_fasta.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from Bio import SeqIO
+
+inputfasta = "file.fasta"
+acronym_pre = "Frag_" #prefix acronym for the outputs
+sizefile = 50000 #number of sequences per file
+countseq = 0
+output = 1
+
+outfasta = open(acronym_pre+str(output)+".fa","w")
+
+with open(inputfasta,"r") as set1:
+    for i in SeqIO.parse(set1,"fasta"):
+        countseq += 1
+        if countseq <= sizefile:
+            output = output
+        else:
+            output += 1
+            countseq = 0
+            outfasta = open("SPfrag_"+str(output)+".fa","w")
+        
+        outfasta.write(">"+str(i.id)+"\n"+str(i.seq)+"\n")
