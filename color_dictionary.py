b1b0a5b8ba751c922495a87ce598f9cfadf91814
Hugo Rody <hugorody@usp.br>
2020-08-26 07:47:42 -0700

    Create color_dictionary.py

---
+++ color_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import matplotlib
+
+rgb_colors = {}
+for name, hex in matplotlib.colors.cnames.items():
+    rgb_colors[name] = matplotlib.colors.to_rgb(hex)
+
+print(rgb_colors)
