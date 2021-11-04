6c99ae32235eb721713f410a84d698c58caaaf11
Hugo Rody <hugorody@usp.br>
2021-11-04 09:20:58 -0300

    Update retrieve_fasta_from_ncbi_using_list.py

--- retrieve_fasta_from_ncbi_using_list.py
+++ retrieve_fasta_from_ncbi_using_list.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from Bio import Entrez
-from urllib2 import HTTPError
+#from urllib2 import HTTPError
 import time
 import sys
 
@@ -10,13 +10,20 @@ fileheaders = sys.argv[1]
 Entrez.email ="eigtw59tyjrt403@gmail.com"
 f = open(fileheaders)
 
-for line in iter(f):
+headers = {}
+with f as set1:
+    for i in set1:
+        i = i.rstrip().split("\t")
+        headers[i[0]] = ''
+
+
+for line in headers.keys():
     try:
         handle = Entrez.efetch(db="protein", id=line, retmode="xml")
     except HTTPError:
         time.sleep(20)
         handle = Entrez.efetch(db="protein", id=line, retmode="xml")
     records = Entrez.read(handle)
-    print ">"+line.rstrip()+" "+records[0]["GBSeq_primary-accession"]+" "+records[0]["GBSeq_definition"]+"\n"+records[0]["GBSeq_sequence"]
-    #time.sleep(1) # to make sure not many requests go per second to ncbi
+    print (">"+line+" "+records[0]["GBSeq_primary-accession"]+" "+records[0]["GBSeq_definition"]+"\n"+records[0]["GBSeq_sequence"].upper())
+    time.sleep(1) # to make sure not many requests go per second to ncbi
 f.close()
